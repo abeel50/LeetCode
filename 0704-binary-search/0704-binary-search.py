6493edf63124ef0b3f7c3ffb09c74859c33d7661
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-13 23:13:12 +0500

    Time: 193 ms (78.10%), Space: 14.5 MB (96.72%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def binarySearch(array, x, low, high):
+
+            if high >= low:
+
+                mid = low + (high - low)//2
+
+                # If found at mid, then return it
+                if array[mid] == x:
+                    return mid
+
+                # Search the left half
+                elif array[mid] > x:
+                    return binarySearch(array, x, low, mid-1)
+
+                # Search the right half
+                else:
+                    return binarySearch(array, x, mid + 1, high)
+
+            else:
+                return -1
+        return binarySearch(nums,target,0,len(nums)-1)
+            
