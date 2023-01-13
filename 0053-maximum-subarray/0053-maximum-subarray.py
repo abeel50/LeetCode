c717c547b4dcd37b8f267a01b2755c88dd5e34bc
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-13 12:55:24 +0500

    Time: 606 ms (68.25%), Space: 26.4 MB (12.81%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if len(nums) == 1: return nums[0]
+        
+        cur_max, max_till_now = 0, float('-inf')
+        for c in nums:
+            cur_max = max(c, cur_max + c)
+            max_till_now = max(max_till_now, cur_max)
+        return max_till_now
+        
