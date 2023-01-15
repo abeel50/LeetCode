9f3c75ed32499393e5ce54e2b0f3fb99574ade84
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-15 12:28:02 +0500

    Time: 15 ms (93.05%), Space: 13.6 MB (11.26%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def areAlmostEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        idx1 ,idx2 = -1, -1
+        for i in range (len(s1)):
+            if s1[i] != s2[i]:
+                if idx1 == -1:
+                    idx1 = i
+                elif idx2 == -1:
+                    idx2 = i
+                else:
+                    return False
+        
+        
+        if (idx1 < 0 and idx2 < 0): return True
+        if (idx2 < 0): return False
+        if (s1[idx1] == s2[idx2] and s1[idx2] == s2[idx1]): return True
+        return False             
+        
