99e69a3a5bc7c979449791e72c61024430e4e762
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-16 21:41:07 +0500

    Time: 826 ms (63.02%), Space: 14.2 MB (18.19%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res, curr = [], []
+        
+        def kSum(start, target, k):
+            if k != 2:
+                for i in range(start, len(nums) - k + 1):
+                    if i > start and nums[i] == nums[i-1]:
+                        continue
+                    curr.append(nums[i])
+                    kSum(i + 1, target- nums[i] ,k-1)
+                    curr.pop()
+                return
+            l, r = start, len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
+                else:
+                    res.append(curr + [nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+        kSum(0, target, 4)
+        return res
+        
