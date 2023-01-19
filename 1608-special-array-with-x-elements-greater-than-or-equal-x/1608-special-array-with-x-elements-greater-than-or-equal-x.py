b1e49c130741673f47360ee2327c4d30a826fd49
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-19 17:02:25 +0500

    Time: 43 ms (64.34%), Space: 14 MB (13.03%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,9 +1,9 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         nums.sort()
-        for x in range(0,len(nums) + 1):
-            count = len([i for i in nums if i >= x])
-            if count == x:
+        n = len(nums)
+        for x in range(n + 1):
+            if x == len([i for i in nums if i >= x]):
                 return x
         return -1
                 
