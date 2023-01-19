b50f79d6585715db516eddc911a7d48077809a9b
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-19 17:43:36 +0500

    Time: 34 ms (92.93%), Space: 13.9 MB (74.37%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,31 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         def mergeLists(l1,l2):
-            temp = None
-            if l1 is None:
-                return l2
-            if l2 is None:
-                return l1
-            if l1.val <= l2.val:
-                temp = ListNode(l1.val)
-                temp.next = mergeLists(l1.next, l2)
-            else:
-                temp = ListNode(l2.val)
-                temp.next = mergeLists(l1, l2.next)
-            return temp
+                temp = None
+                if l1 is None:
+                    return l2
+                if l2 is None:
+                    return l1
+                if l1.val <= l2.val:
+                    temp = ListNode(l1.val)
+                    temp.next = mergeLists(l1.next, l2)
+                else:
+                    temp = ListNode(l2.val)
+                    temp.next = mergeLists(l1, l2.next)
+                return temp
         return mergeLists(list1,list2)
-
-                
-                    
         
