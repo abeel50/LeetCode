84188d1f8cb192901d2745f552020b3ce5eda28a
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-21 15:28:50 +0500

    Time: 69 ms (62.78%), Space: 15.7 MB (47.91%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -11,20 +11,29 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         
+        #recursive function
         def rec(root):
             if not root: return
+            
+            # check if it's leaf node
             if root.left and root.right:
+                
+                #connects left--> right
                 root.left.next = root.right
 
+                #left and right nodes
                 left, right = root.left, root.right
                 
-                if left and left.right and right and right.left:
+                # to connect left side's rightmost node to right side's leftmoste
+                if left.right and right.left:
                     temp = left
                     while temp.next:
                         temp.right.next = temp.next.left
                         temp = temp.next
+                
                 rec(root.left)
                 rec(root.right)
+            return
         rec(root)
         return root
         
