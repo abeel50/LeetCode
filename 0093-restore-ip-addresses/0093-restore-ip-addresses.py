8ff9917fb0bc994297bc3410b797293be1a6ae00
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-21 11:14:46 +0500

    Time: 41 ms (67.61%), Space: 13.8 MB (99.19%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,23 @@
+
+class Solution:
+
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        def leading_0(st):
+            return st[0] == "0" and st != "0"
+
+        def valid(part):
+            return not leading_0(part) and int(part) <= 255
+
+        def valid_ip(nums):
+            return all(valid(part) for part in nums)
+
+        ret = []
+        for i, j, k in itertools.combinations(range(1, len(s)), 3):
+            ip = [s[:i], s[i:j], s[j:k], s[k:]]
+            if valid_ip(ip):
+                ret.append(".".join(ip))
+        return ret
+
+            
+        
