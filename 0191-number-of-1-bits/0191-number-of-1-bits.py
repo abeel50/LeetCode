29e95108ec7bb924ad1db84f06a0c158da4838ed
Abeel <37096740+abeel50@users.noreply.github.com>
2023-01-25 11:24:50 +0500

    Time: 42 ms (38.87%), Space: 13.9 MB (6.35%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def hammingWeight(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def hammingWeight(self, n: int) -> int:
         c = 0
         for i in str(bin(n)[2:]):
             if int(i) == 1:
