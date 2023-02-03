53efe5a3d71540d45e030fae0a43cf3a6c8c60c7
Abeel <37096740+abeel50@users.noreply.github.com>
2023-02-03 10:43:36 +0500

    Time: 812 ms (47.55%), Space: 27.4 MB (46.11%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
         res = float('-inf')
         
         #start with two pointers one with start one with end
@@ -11,15 +7,11 @@ class Solution(object):
         
         while start < end:
             water = (end - start) * min(height[start],height[end])
-            if water > res:
-                res = water
+            res = max(res, water)
             #Move pointer that is low
             if height[start] < height[end]:
                 start += 1
             else:
                 end -= 1
         return res
-            
-
-            
         
