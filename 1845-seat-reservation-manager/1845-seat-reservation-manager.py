4026e9f5d0b94cd1337e30f244dbe3e3cae2365a
Abeel <37096740+abeel50@users.noreply.github.com>
2023-02-07 23:29:43 +0500

    Time: 690 ms (32.16%), Space: 51.1 MB (7.54%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -1,27 +1,35 @@
 class SeatManager:
 
     def __init__(self, n: int):
-        self.SIZE, self.booked= n, 0
         self.reserved = defaultdict(bool)
-        self.batch = 10
-        self.start , self.end = 1 , self.batch
         self.available = []
+        self.SIZE, self.booked= n, 0
+        self.batch = 100
+        self.start, self.end = 1, self.batch
         self.setHeap()
         
+    # sets heap according to constraints
     def setHeap(self):
+        # if heap size is less than Batch size
+        # then heap Size is MAx number of seats
         if self.SIZE < self.batch:
             self.available = [*range(1, self.SIZE + 1)]
         else:
+            #initilize heap batch
             self.available = [*range(self.start, self.end + 1)]
             self.start = self.end + 1
             self.end += self.batch
         heapq.heapify(self.available)
-    
+        
+    # if all seats are booked 
     def isFull(self):
         return self.booked == self.SIZE
     
+    # adds heap batch
     def addHeapBatch(self):
+        # if no more seats
         if self.isFull(): return
+        # if any space is available in previous batch
         if len(self.available) > 0: return
         self.available = [*range (self.start, self.end + 1)]
         self.start = self.end + 1
