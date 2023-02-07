fa4f9e8976be5b883a533c58f0e353e79e316cb3
Abeel <37096740+abeel50@users.noreply.github.com>
2023-02-07 12:28:14 +0500

    Time: 70 ms (69.12%), Space: 14 MB (36.55%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,34 +8,34 @@ class Solution:
         head = None
         p = None
         c = 0
-        while l1 != None or l2 != None:
-            if l1 is None:
-                res = l2.val
-            elif l2 is None:
-                res = l1.val
+        while l1 or l2:
+            if not l1:
+                res = l2.val + c
+            elif not l2:
+                res = l1.val + c
             else:
-                res = l1.val + l2.val
-            if c == 1: res += 1
+                res = l1.val + l2.val + c
+                
+            # if ther's a carry
             if res > 9:
                 res = int(str(res)[1])
                 c = 1
             else:
                 c = 0
-            if head is None:
+                
+            if not head:
                 head = ListNode(res)
                 p = head
             else:
                 p.next = ListNode(res)
                 p = p.next
                 
-            if l1 is not None:
+            if l1:
                 l1 = l1.next
-            if l2 is not None:
+            if l2:
                 l2 = l2.next
         if c == 1:
             p.next = ListNode(c)
 
-
-            
         return head
         
