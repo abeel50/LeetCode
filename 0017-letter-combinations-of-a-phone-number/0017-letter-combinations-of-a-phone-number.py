211a2b41269a19d2ba1b6c9c3cb5ec27d65f0860
Abeel <37096740+abeel50@users.noreply.github.com>
2023-02-10 23:39:32 +0500

    Time: 38 ms (35.69%), Space: 13.8 MB (98.58%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
         d  ={"2":['a','b','c'], "3":['d','e','f'],
             "4":['g','h','i'], "5":['j','k','l'], "6":['m','n','o'],
             "7":['p','q','r','s'], "8":['t','u','v'], "9":['w','x','y','z']}
@@ -15,5 +11,3 @@ class Solution(object):
             res = [str(x) + str(y) for x in res for y in d[digits[i]]]
         
         return res
-
-        
