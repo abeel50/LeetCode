45cbab320123c858473a50af71437af2285efad7
Abeel <37096740+abeel50@users.noreply.github.com>
2023-02-14 17:40:35 +0500

    Time: 35 ms (61.60%), Space: 14 MB (33.71%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isSubsequence(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) == 0: return True
         i, j ,match= 0, 0, 0
         while i< len(s) and j < len(t):
@@ -12,7 +7,5 @@ class Solution(object):
                 i += 1
                 match += 1
             j += 1
-        return len(s) == match
-        
-            
+        return len(s) == match     
         
