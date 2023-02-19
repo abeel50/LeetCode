128010be4b35e854ac72d24619b74132b60aa79f
Abeel <37096740+abeel50@users.noreply.github.com>
2023-02-19 08:26:39 +0500

    Time: 41 ms (72.05%), Space: 14.2 MB (18.44%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -4,38 +4,21 @@ class Solution:
         used = [False] * len(nums)
 
         def dfs(path: List[int]) -> None:
-          if len(path) == len(nums):
-            ans.append(path.copy())
-            return
-
-          for i, num in enumerate(nums):
-            if used[i]:
-              continue
-            used[i] = True
-            path.append(num)
-            dfs(path)
-            path.pop()
-            used[i] = False
+            if len(path) == len(nums):
+                ans.append(path.copy())
+                return
+            for i, num in enumerate(nums):
+                if used[i]:
+                    continue
+                used[i] = True
+                path.append(num)
+                dfs(path)
+                path.pop()
+                used[i] = False
 
         dfs([])
         return ans
-#         res = []
-        
-#         if len(nums) == 1:
-#             return [nums[:]]
-#         print(res)
 
-#         for i in range(len(nums)):
-#             n = nums.pop(0)
-#             print(nums)
-#             perms = self.permute(nums)
-
-#             for p in perms:
-#                 perms.append(n)
-#             res.extend(perms)
-#             print(res)
-#             nums.append(n)
-#         return res
 
             
         
