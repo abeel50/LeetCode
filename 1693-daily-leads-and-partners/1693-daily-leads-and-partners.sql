4754dfe072b0b62d1af22c0f2221cea3eae24fcc
Abeel <37096740+abeel50@users.noreply.github.com>
2023-03-03 09:35:18 +0500

    Time: 857 ms (81.03%), Space: 0B (100.00%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select date_id, make_name, 
+    count(distinct lead_id) unique_leads,
+    count(distinct partner_id) unique_partners
+from DailySales
+group by 1,2
